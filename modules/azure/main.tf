9261e979cda8fbb12f368b542100cbdc21bb27d2
Shalineel <96731437+Shalineel@users.noreply.github.com>
2021-12-31 17:37:48 +0530

    Update main.tf

--- modules/azure/main.tf
+++ modules/azure/main.tf
@@ -5,13 +5,6 @@ terraform {
       version = "=2.46.0"
     }
   }
-    backend "azurerm" {
-        resource_group_name  = "demo"
-        storage_account_name = "shaluneel18"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-    }
-
 }
 provider "azurerm" {
   features {}
